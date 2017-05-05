65d680d21818f02efe624683fa2ea383fac8a661
Jeremy Grace <jeremy.grace@outlook.com>
2017-05-04 20:35:01 -0700

    Refactor code for checkboard shaping and calibration function error

--- raspberryPi/picamera_calibrate.py
+++ raspberryPi/picamera_calibrate.py
@@ -17,38 +17,37 @@ import glob2
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-obj_pt = np.zeros((6 * 7, 3), np.float32)
-obj_pt[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+obj_pt = np.zeros((6 * 9, 3), np.float32)
+obj_pt[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 object_points = []  # 3d point in real world space
 img_points = []  # 2d points in image plane.
+h, w = 0, 0
 
+chessbd_imgs = glob2.glob('chessboard/*.jpg')
 
-checkbd_imgs = glob2.glob('/checkboard/*.jpg')
-
-for fname in checkbd_imgs:
+for fname in chessbd_imgs:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    h, w = gray.shape[:2]
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
 
     # If found, add object points, image points (after refining them)
     if ret:
-        corners2 = cv2.cornerSubPix(gray,
-                                    corners,
-                                    (11, 11), (-1, -1), criteria)
-        img_points.append(corners2)
+        cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        img_points.append(corners)
         object_points.append(obj_pt)
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv2.drawChessboardCorners(img, (9, 6), corners, ret)
         cv2.imshow('image', img)
         cv2.waitKey(500)
 # calibration
-ret, mtx, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(object_points,
-                                                         img_points,
-                                                         gray.shape[::-1],
-                                                         None, None)
+retval, mtx, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(object_points,
+                                                            img_points,
+                                                            (w, h),
+                                                            None, None)
 
 print("Camera Matrix:\n", mtx)
 
