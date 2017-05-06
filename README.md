c4ad9c73ad4d1346bd2a4557d84c2e1bd0bb16b7
Jeremy Grace <jeremy.grace@outlook.com>
2017-05-06 00:25:08 -0700

    Add macOS .py files and streaming_client for raspberry pi

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 <div style='float:left; width:30%'>
 <h4>Hardware</h4>
 <ul>
+    <li><a href="https://aws.amazon.com/ec2/instance-types/p2/">AWS EC2 P2 instance [ 1 NVIDIA K80 GPU ]</li>
     <li><a href="https://www.apple.com/macos/sierra/">macOS Sierra</a></li>
     <li><a href="https://www.dexterindustries.com/GoPiGo/">Dexters Industries GoPiGo</a></li>
       <ul>
@@ -22,6 +23,15 @@
 <div style='float:left; width:40%; margin-left:30px'>
 <h4>Software Dependencies</h4>
 <ul>
+  <li>Bitfusion Ubuntu 14 Tensorflow - Ubuntu 14.04 LTS:
+  <ul>
+    <li><a href="https://www.digitalocean.com/community/tutorials/how-to-install-the-anaconda-python-distribution-on-ubuntu-16-04">Anaconda 4.3</a> : Python 3.5 env</li>
+    <li>Numpy 1.12</li>
+    <li>TensorFlow 1.0 + Keras 2.0</li>
+    <li><a href="http://opencv.org/">OpenCV</a> 3.1</li>
+    <li><a href="https://www.pygame.org/docs/)">Pygame</a> 1.9</li>
+    <li><a href="https://pypi.python.org/pypi/pyserial">PySerial</a> 3.3</li>
+  </ul>
   <li>macOS:
   <ul>
     <li><a href="https://www.continuum.io/anaconda-overview">Anaconda 4.3</a> : Python 3.5 env</li>
---
+++ raspberryPi-GoPiGo/streaming_client.py
@@ -0,0 +1,42 @@
+"""
+Reference:
+PiCamera documentation
+https://picamera.readthedocs.org/en/release-1.10/recipes2.html
+
+"""
+
+import io
+import socket
+import struct
+import time
+import picamera
+
+
+# Create socket and bind host
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('10.3.34.153', 8000))
+connection = client.makefile('wb')
+
+try:
+    with picamera.PiCamera() as cam:
+        cam.resolution = (320, 240)  # picamera resolution
+        cam.framerate = 10  # 10 frames/sec
+        time.sleep(2)  # Hold for 2 secs in order for camera to initialize
+        start = time.time()
+        stream = io.BytesIO()
+
+        # Send jpg formatted video stream
+        for _ in cam.capture_continuous(stream, 'jpg', use_video_port=True):
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+            stream.seek(0)
+            connection.write(stream.read())
+            if time.time() - start > 600:
+                break
+            stream.seek(0)
+            stream.truncate()
+    connection.write(struct.pack('<L', 0))
+
+finally:
+    connection.close()
+    client.close()
